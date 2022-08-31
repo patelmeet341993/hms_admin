2e4ededc56befa9ebf62dfa34131730d4ccf2332
Dishant <agrawaldishant29@gmail.com>
2022-09-01 01:08:19 +0530

    added ConnectionProvider class to track connection status

---
+++ lib/providers/connection_provider.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:admin/utils/logger_service.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/foundation.dart';
+
+class ConnectionProvider extends ChangeNotifier {
+  bool isInternet = true;
+  StreamSubscription<ConnectivityResult>? subscription;
+
+  ConnectionProvider() {
+    Log().d("ConnectionProvider constructor called");
+    if(kIsWeb) {
+      isInternet = true;
+    }
+    else {
+      isInternet = Platform.isIOS;
+    }
+    try {
+      Connectivity().checkConnectivity().then((ConnectivityResult result) {
+        Log().d("Connectivity Result:$result");
+        isInternet = result == ConnectivityResult.none ? false : true;
+
+        Log().d("Connection Subscription Started");
+        subscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) async {
+          Log().d("Connectivity Result:$result");
+          isInternet = result == ConnectivityResult.none ? false : true;
+          notifyListeners();
+        });
+      });
+    }
+    catch (E) {
+      Log().e("Error in Connectivity Subscription:  $E");
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    subscription?.cancel();
+    subscription = null;
+  }
+}
