8caa446d6908deec085c86ca5dbf58ab4895cd52
Dishant <agrawaldishant29@gmail.com>
2022-09-01 00:56:48 +0530

    added theme and color related files

---
+++ lib/providers/app_theme_provider.dart
@@ -0,0 +1,39 @@
+/*
+* File : App Theme Notifier (Listener)
+* Version : 1.0.0
+* */
+
+import 'package:admin/configs/constants.dart';
+import 'package:admin/utils/shared_pref_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppThemeProvider extends ChangeNotifier {
+
+  int _themeMode = 1;
+
+  AppThemeProvider() {
+    init();
+  }
+
+  init() async {
+    int? data =  await SharedPrefManager().getInt(appThemeMode);
+    if(data==null) {
+      _themeMode = 1;
+    }
+    else {
+      _themeMode = data;
+    }
+    notifyListeners();
+  }
+
+  int get themeMode => _themeMode;
+
+  Future<void> updateTheme(int themeMode) async {
+    _themeMode = themeMode;
+    notifyListeners();
+
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setInt("themeMode", themeMode);
+  }
+}
