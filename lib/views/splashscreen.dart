132e1c43d6b0d23a3704a412693d028b52fc21a5
Dishant <agrawaldishant29@gmail.com>
2022-09-01 01:00:55 +0530

    navigated to login screen or home screen in splash screen

--- lib/views/splashscreen.dart
+++ lib/views/splashscreen.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:admin/views/authentication/login_screen.dart';
+import 'package:admin/views/homescreen/homescreen.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class SplashScreen extends StatefulWidget {
   static const String routeName = "/SplashScreen";
@@ -10,8 +15,37 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late ThemeData themeData;
+
+  Future<void> checkLogin() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    if(Random().nextBool()) {
+      Navigator.pushNamedAndRemoveUntil(context, HomeScreen.routeName, (route) => false);
+    }
+    else {
+      Navigator.pushNamedAndRemoveUntil(context, LoginScreen.routeName, (route) => false);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      checkLogin();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    themeData = Theme.of(context);
+
+    return Container(
+      child: Scaffold(
+        body: Center(
+          child: LoadingAnimationWidget.inkDrop(color: themeData.primaryColor, size: 40),
+        ),
+      ),
+    );
   }
 }
