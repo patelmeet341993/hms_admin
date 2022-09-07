9ff39bf38e2467ae25b67135578ca1bb064b0434
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2022-09-07 17:08:38 +0530

    Some fixes 07 09 2022 (#3)
    * added PatientProvider to store Patients List
    
    * Added NewDocumentDataModel to Store New Document id and New Timestamp got from Firestore
    
    * 1) Added DataController for some common operations to get data
    2) added getNewDocIdAndTimeStamp method to get new document id and timestamp
    
    * modified PatientModel
    
    * added PatientController for patient creation, get patients list etc
    
    * added PatientProvider in MultiProvider
    
    * added ConnectionController to check status of internet connection
    
    * added some new texts in App Strings
    
    * added option to customize the LoadingWidget with different parameters
    
    * added instructions key in PrescriptionModel
    
    * some minor changes
    
    * created dashboard screen to show logged in admin user data and logout button
    
    * shown dashboard screen in home screen

--- lib/configs/app_strings.dart
+++ lib/configs/app_strings.dart
@@ -2,6 +2,7 @@ class AppStrings {
   static const String givenUserAlreadyExist = "User with given UserName already exist";
   static const String usernameOrPasswordIsEmpty = "UserName or password is empty";
   static const String login = "Login";
+  static const String no_internet = "No Internet";
 
   // region
   static const String loginScreen = "Login";
