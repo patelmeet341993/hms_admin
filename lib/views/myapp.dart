8a87d7a6bc11f51cb8e5d8f79f1dc6d5034072fc
Dishant <agrawaldishant29@gmail.com>
2022-09-07 16:57:00 +0530

    added PatientProvider in MultiProvider

--- lib/views/myapp.dart
+++ lib/views/myapp.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../controllers/navigation_controller.dart';
+import '../providers/patient_provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider<AppThemeProvider>(create: (_) => AppThemeProvider(), lazy: false),
         ChangeNotifierProvider<ConnectionProvider>(create: (_) => ConnectionProvider(), lazy: false),
         ChangeNotifierProvider<AdminUserProvider>(create: (_) => AdminUserProvider(), lazy: false),
+        ChangeNotifierProvider<PatientProvider>(create: (_) => PatientProvider(), lazy: false),
       ],
       child: MainApp(),
     );
