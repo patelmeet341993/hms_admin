fe4ffb578aa130ac97ba159fb98010d3e8278b38
Dishant <agrawaldishant29@gmail.com>
2022-09-08 17:00:18 +0530

    modified Admin Users List Screen with neat table

--- lib/views/admin_users/admin_users_list_screen.dart
+++ lib/views/admin_users/admin_users_list_screen.dart
@@ -3,8 +3,12 @@ import 'package:admin/controllers/admin_user_controller.dart';
 import 'package:admin/models/admin_user_model.dart';
 import 'package:admin/providers/admin_user_provider.dart';
 import 'package:admin/views/common/components/loading_widget.dart';
+import 'package:admin/views/common/components/my_table/my_table_cell_model.dart';
+import 'package:admin/views/common/components/my_table/my_table_row_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../../utils/SizeConfig.dart';
 
 class AdminUsersListScreen extends StatefulWidget {
   const AdminUsersListScreen({Key? key}) : super(key: key);
@@ -57,45 +61,33 @@ class _AdminUsersListScreenState extends State<AdminUsersListScreen> with Automa
   }
 
   Widget getMainBody(List<AdminUserModel> users) {
-    flexes = [1, 3, 3, 3, 2, 1];
+    flexes = [1, 3, 3, 3, 2, 1, 1];
+    List<String> titles = ["Sr No.", "Id", "Name", "Role", "Username", "Active", "Edit"];
+
+    Color textColor = themeData.colorScheme.onPrimary;
+    FontWeight fontWeight = FontWeight.w800;
+    double fontSize = 16;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
         children: [
-          getTableRowWidget(
-            [
-              "Sr No.",
-              "Id",
-              "Name",
-              "Role",
-              "Username",
-              "Active",
-            ],
+          MyTableRowWidget(
             backgroundColor: themeData.colorScheme.primary,
-            textColor: themeData.colorScheme.onPrimary,
-            fontWeight: FontWeight.w700,
-          ),
-          /*Row(
-            children: [
-              Expanded(
-                flex: flexes[0],
-                child: getTableColumnTitleWidget("Id"),
-              ),
-              Expanded(
-                flex: flexes[1],
-                child: getTableColumnTitleWidget("Name"),
-              ),
-              Expanded(
-                flex: flexes[2],
-                child: getTableColumnTitleWidget("Role"),
-              ),
-              Expanded(
-                flex: flexes[3],
-                child: getTableColumnTitleWidget("Username"),
-              ),
+            cells: [
+              ...List.generate(titles.length, (index) {
+                return MyTableCellModel(
+                  flex: flexes[index],
+                  child: getTableCellWidget(
+                    titles[index],
+                    textColor: textColor,
+                    fontWeight: fontWeight,
+                    size: fontSize,
+                  ),
+                );
+              }),
             ],
-          ),*/
+          ),
           Expanded(child: getAdminUsersListView(users))
         ],
       ),
@@ -119,87 +111,97 @@ class _AdminUsersListScreenState extends State<AdminUsersListScreen> with Automa
   }
 
   Widget getAdminUserWidget(AdminUserModel adminUserModel, int index) {
+    FontWeight? fontWeight = FontWeight.w600;
+
     return Container(
       margin: EdgeInsets.symmetric(vertical: 5),
-      child: getTableRowWidget(
-          [
-          (index + 1).toString(),
-          adminUserModel.id,
-          adminUserModel.name,
-          adminUserModel.role,
-          adminUserModel.username,
-          adminUserModel.isActive ? "Activated" : "Deactivated",
-        ],
-        fontWeight: FontWeight.w600,
-      ),
-    );
-    /*return Row(
-      children: [
-        Expanded(
-          flex: flexes[0],
-          child: getTableColumnTitleWidget(adminUserModel.id),
-        ),
-        Expanded(
-          flex: flexes[1],
-          child: getTableColumnTitleWidget(adminUserModel.name),
-        ),
-        Expanded(
-          flex: flexes[2],
-          child: getTableColumnTitleWidget(adminUserModel.role),
-        ),
-        Expanded(
-          flex: flexes[3],
-          child: getTableColumnTitleWidget(adminUserModel.username),
-        ),
-      ],
-    );*/
-  }
-
-  Widget getTableRowWidget(List<String> values, {Color? backgroundColor, Color? textColor, FontWeight? fontWeight}) {
-    return Container(
-      // margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-      decoration: BoxDecoration(
-        color: backgroundColor ?? themeData.cardColor,
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: Row(
-        children: List.generate(values.length, (index) {
-          return Expanded(
-            flex: flexes[index],
-            child: getTableColumnTitleWidget(values[index], textColor: textColor, fontWeight: fontWeight),
-          );
-        }),
-        /*children: [
-          Expanded(
+      child: MyTableRowWidget(
+        cells: [
+          MyTableCellModel(
             flex: flexes[0],
-            child: getTableColumnTitleWidget(values[0]),
+            child: getTableCellWidget((index + 1).toString(), fontWeight: fontWeight),
           ),
-          Expanded(
+          MyTableCellModel(
             flex: flexes[1],
-            child: getTableColumnTitleWidget(values[1]),
+            child: getTableCellWidget(adminUserModel.id, fontWeight: fontWeight),
           ),
-          Expanded(
+          MyTableCellModel(
             flex: flexes[2],
-            child: getTableColumnTitleWidget(values[2]),
+            child: getTableCellWidget(adminUserModel.name, fontWeight: fontWeight),
           ),
-          Expanded(
+          MyTableCellModel(
             flex: flexes[3],
-            child: getTableColumnTitleWidget(values[3]),
+            child: getTableCellWidget(adminUserModel.role, fontWeight: fontWeight),
           ),
-        ],*/
+          MyTableCellModel(
+            flex: flexes[4],
+            child: getTableCellWidget(adminUserModel.username, fontWeight: fontWeight),
+          ),
+          MyTableCellModel(
+            flex: flexes[5],
+            child: getTableCellWidget(adminUserModel.isActive ? "Activated" : "Deactivated", fontWeight: fontWeight),
+          ),
+          MyTableCellModel(
+            flex: flexes[6],
+            child: getEditDeleUser(),
+          ),
+        ],
       ),
     );
   }
 
-  Widget getTableColumnTitleWidget(String title, {Color? textColor, FontWeight? fontWeight}) {
+  Widget getTableCellWidget(String title, {Color? textColor, FontWeight? fontWeight, double? size}) {
     return Text(
       title,
       style: themeData.textTheme.caption?.copyWith(
         color: textColor,
         fontWeight: fontWeight,
+        fontSize: size,
       ),
       textAlign: TextAlign.center,
+    );
+  }
+
+  Widget getEditDeleUser() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        getIconButton(
+          iconData: Icons.edit,
+          onTap: () {
+
+          },
+        ),
+        SizedBox(width: 5,),
+        getIconButton(
+          iconData: Icons.delete,
+          onTap: () {
+
+          },
+          backgroundColor: Colors.red,
+        ),
+      ],
+    );
+  }
+
+  Widget getIconButton({required IconData iconData, void Function()? onTap, Color? backgroundColor, Color? iconColor,}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: Spacing.all(6),
+        decoration: BoxDecoration(
+          color: backgroundColor ?? themeData.colorScheme.primary,
+          borderRadius: const BorderRadius.all(Radius.circular(8),),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Icon(
+            iconData,
+            color: iconColor ?? themeData.colorScheme.onPrimary,
+            size: 20,
+          ),
+        ),
+      ),
     );
   }
 
