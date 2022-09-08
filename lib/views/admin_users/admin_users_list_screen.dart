28a03e41a73116210c71a75d30a703b4d3b71a9a
Dishant <agrawaldishant29@gmail.com>
2022-09-08 12:52:56 +0530

    addd AdminUsersListScreen

---
+++ lib/views/admin_users/admin_users_list_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AdminUsersListScreen extends StatefulWidget {
+  const AdminUsersListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AdminUsersListScreen> createState() => _AdminUsersListScreenState();
+}
+
+class _AdminUsersListScreenState extends State<AdminUsersListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        body: SizedBox(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
