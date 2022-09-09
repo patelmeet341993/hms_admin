bb6d94e3631383cb8a9993583de3ade0b76e4bd1
Dishant <agrawaldishant29@gmail.com>
2022-09-09 22:19:31 +0530

    1) Improved Add Admin User in Firestore 2) Added Pagination Functionality in GetAdminUsers

--- lib/controllers/admin_user_controller.dart
+++ lib/controllers/admin_user_controller.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:admin/controllers/data_controller.dart';
 import 'package:admin/controllers/navigation_controller.dart';
+import 'package:admin/models/new_document_data_model.dart';
 import 'package:admin/providers/admin_user_provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -46,7 +48,13 @@ class AdminUserController {
         description: userModel.description,
         imageUrl: userModel.imageUrl,
         scannerData: userModel.scannerData,
+        isActive: true,
+        createdTime: Timestamp.now(),
       );
+
+      NewDocumentDataModel newDocumentDataModel = await DataController().getNewDocIdAndTimeStamp();
+      adminUserModel.id = newDocumentDataModel.docid;
+      adminUserModel.createdTime = newDocumentDataModel.timestamp;
 
       bool isCreationSuccess = await FirestoreController().firestore.collection(FirebaseNodes.adminUsersCollection).doc(adminUserModel.id).set(adminUserModel.toMap()).then((value) {
         Log().i("Admin User with Id:${adminUserModel!.id} Created Successfully");
@@ -71,25 +79,41 @@ class AdminUserController {
     }
   }
 
-  Future<bool> deleteAdminUser(String adminUserId) async {
-    Log().i("deleteAdminUser called with adminUserId: $adminUserId");
+  Future<void> addAdminUserInFirestoreAndUpdateInProvider({required BuildContext context, required AdminUserModel adminUserModel}) async {
+    AdminUserProvider adminUserProvider = Provider.of<AdminUserProvider>(NavigationController.mainScreenNavigator.currentContext!, listen: false);
+
+    AdminUserModel? newAdminUserModel = await AdminUserController().createAdminUserWithUsernameAndPassword(context: context, userModel: adminUserModel);
+    if(newAdminUserModel != null) {
+      adminUserProvider.addAdminUsersInList([newAdminUserModel]);
+    }
+  }
+
+  Future<bool> deleteAdminUsers(List<String> adminUserIds) async {
+    Log().i("deleteAdminUser called with adminUserIds: $adminUserIds");
 
     bool isDeleted = false;
 
-    if(adminUserId.isNotEmpty) {
-      isDeleted = await FirestoreController().firestore.collection(FirebaseNodes.adminUsersCollection).doc(adminUserId).delete().then((value) {
-        Log().i("Deleted Admin User with Id: $adminUserId");
+    adminUserIds.removeWhere((element) => element.isEmpty);
+    if(adminUserIds.isNotEmpty) {
+      WriteBatch writeBatch = FirestoreController().firestore.batch();
+
+      for (String id in adminUserIds) {
+        writeBatch.delete(FirestoreController().firestore.collection(FirebaseNodes.adminUsersCollection).doc(id));
+      }
+
+      isDeleted = await writeBatch.commit().then((value) {
+        Log().i("Deleted Admin User with Ids: $adminUserIds");
         return true;
       })
       .catchError((e, s) {
-        Log().e("Error in Deleting Admin User with Id '$adminUserId':$e", s);
+        Log().e("Error in Deleting Admin User with Id '$adminUserIds':$e", s);
         return false;
       });
     }
 
     if(isDeleted) {
       AdminUserProvider adminUserProvider = Provider.of<AdminUserProvider>(NavigationController.mainScreenNavigator.currentContext!, listen: false);
-      adminUserProvider.setAdminUsers(adminUserProvider.adminUsers..removeWhere((element) => element.id == adminUserId));
+      adminUserProvider.setAdminUsers(adminUserProvider.adminUsers..removeWhere((element) => adminUserIds.contains(element.id)));
     }
 
     return isDeleted;
@@ -136,7 +160,7 @@ class AdminUserController {
     adminUserProvider.setAdminUserModel(null);
   }
 
-  Future<List<AdminUserModel>> getAdminUsers({bool isRefresh = true, bool isFromCache = false}) async {
+  Future<List<AdminUserModel>> getAdminUsers({bool isRefresh = true, bool isFromCache = false, bool isNotify = true}) async {
     Log().i("getAdminUsers called with isRefresh:$isRefresh, isFromCache:$isFromCache");
     AdminUserProvider adminUserProvider = Provider.of<AdminUserProvider>(NavigationController.mainScreenNavigator.currentContext!, listen: false);
 
@@ -145,19 +169,64 @@ class AdminUserController {
       return adminUserProvider.adminUsers;
     }
 
-    QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirestoreController().firestore.collection(FirebaseNodes.adminUsersCollection).get();
-    Log().i("Documents Length in Firestore for Admin Users:${querySnapshot.docs.length}");
-
-    List<AdminUserModel> list = [];
-    for (DocumentSnapshot<Map<String, dynamic>> documentSnapshot in querySnapshot.docs) {
-      if((documentSnapshot.data() ?? {}).isNotEmpty) {
-        AdminUserModel adminUserModel = AdminUserModel.fromMap(documentSnapshot.data()!);
-        list.add(adminUserModel);
-      }
+    if (isRefresh) {
+      Log().d("Refresh");
+      adminUserProvider.setHasMoreUsers = true; // flag for more products available or not
+      adminUserProvider.setLastDocument = null; // flag for last document from where next 10 records to be fetched
+      adminUserProvider.setIsUsersLoading(false, isNotify: isNotify);
+      adminUserProvider.setAdminUsers([], isNotify: isNotify);
     }
-    adminUserProvider.setAdminUsers(list);
-    Log().i("Final AdminUsers Length From Firestore:${list.length}");
-    Log().i("Final AdminUsers Length in Provider:${adminUserProvider.adminUsersLength}");
-    return list;
+
+    try {
+      if (!adminUserProvider.getHasMoreUsers) {
+        Log().d('No More Users');
+        return adminUserProvider.adminUsers;
+      }
+      if (adminUserProvider.getIsUsersLoading) return adminUserProvider.adminUsers;
+
+      adminUserProvider.setIsUsersLoading(true, isNotify: isNotify);
+
+      Query<Map<String, dynamic>> query = FirestoreController()
+        .firestore
+        .collection(FirebaseNodes.adminUsersCollection)
+        .limit(AppConstants.adminUsersDocumentLimitForPagination);
+
+      //For Last Document
+      DocumentSnapshot<Map<String, dynamic>>? snapshot = adminUserProvider.getLastDocument;
+      if(snapshot != null) {
+        Log().d("LastDocument not null");
+        query = query.startAfterDocument(snapshot);
+      }
+      else {
+        Log().d("LastDocument null");
+      }
+
+      QuerySnapshot<Map<String, dynamic>> querySnapshot = await query.get();
+      Log().i("Documents Length in Firestore for Admin Users:${querySnapshot.docs.length}");
+
+      if (querySnapshot.docs.length < AppConstants.adminUsersDocumentLimitForPagination) adminUserProvider.setHasMoreUsers = false;
+
+      if(querySnapshot.docs.isNotEmpty) adminUserProvider.setLastDocument = querySnapshot.docs[querySnapshot.docs.length - 1];
+
+      List<AdminUserModel> list = [];
+      for (DocumentSnapshot<Map<String, dynamic>> documentSnapshot in querySnapshot.docs) {
+        if((documentSnapshot.data() ?? {}).isNotEmpty) {
+          AdminUserModel adminUserModel = AdminUserModel.fromMap(documentSnapshot.data()!);
+          list.add(adminUserModel);
+        }
+      }
+      adminUserProvider.addAdminUsersInList(list, isNotify: false);
+      adminUserProvider.setIsUsersLoading(false);
+      Log().i("Final AdminUsers Length From Firestore:${list.length}");
+      Log().i("Final AdminUsers Length in Provider:${adminUserProvider.adminUsersLength}");
+      return list;
+    }
+    catch(e, s) {
+      Log().e("Error in AdminUserController().getAdminUsers():$e", s);
+      adminUserProvider.setHasMoreUsers = true;
+      adminUserProvider.setLastDocument = null;
+      adminUserProvider.setAdminUsers([], isNotify: false);
+      return [];
+    }
   }
 }
