91b6d9f58d49ea101dd6673e6ec3cecfd93b4705
Dishant <agrawaldishant29@gmail.com>
2022-09-12 12:48:08 +0530

    added some new Strings in AppStrings

--- lib/configs/app_strings.dart
+++ lib/configs/app_strings.dart
@@ -5,6 +5,17 @@ class AppStrings {
   static const String no_internet = "No Internet";
   static const String no_users = "No Users";
   static const String are_you_sure_want_to_delete_admin_user = "Are you sure want to delete this user?";
+  static const String role_is_required = "Role is Required*";
+  static const String edit = "Edit";
+  static const String cancel = "Cancel";
+  static const String create = "Create";
+  static const String name = "Name";
+  static const String username = "UserName";
+  static const String password = "Password";
+  static const String description = "Description";
+  static const String active = "Active";
+  static const String role = "Role";
+  static const String select_role = "Select Role";
 
   // region
   static const String loginScreen = "Login";
