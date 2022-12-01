7323a41f601bab879bc4c6e0baa71476a3442536
Dishant <agrawaldishant29@gmail.com>
2022-12-01 12:53:58 +0530

    added Scanner Screen in Home Screen Tab

--- lib/configs/constants.dart
+++ lib/configs/constants.dart
@@ -2,9 +2,12 @@ import 'package:admin/views/admin_users/admin_users_list_screen.dart';
 import 'package:admin/views/common/models/home_screen_component_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hms_models/configs/constants.dart';
+import 'package:hms_models/hms_models.dart';
 
+import '../views/Scanner/scanner.dart';
 import '../views/homescreen/dashboard_screen.dart';
 import '../views/profile/admin_user_profile_screen.dart';
+import 'app_strings.dart';
 
 //App Version
 const String app_version = "1.0.0";
@@ -41,6 +44,7 @@ class HomeScreenComponentsList {
     const HomeScreenComponentModel(icon: Icons.dashboard_outlined, activeIcon: Icons.dashboard, screen: DashboardScreen(), title: "Dashboard"),
     const HomeScreenComponentModel(icon: Icons.history, activeIcon: Icons.history, screen: Text("History"), title: "History"),
     const HomeScreenComponentModel(icon: Icons.file_copy_outlined, activeIcon: Icons.file_copy, screen: Text("Treatment"), title: "Treatment"),
+    const HomeScreenComponentModel(icon: FontAwesomeIcons.qrcode, activeIcon: FontAwesomeIcons.qrcode, screen: ScannerScreen(), title: AppStrings.scanner),
     const HomeScreenComponentModel(icon: Icons.people_alt_outlined, activeIcon: Icons.people, screen: AdminUsersListScreen(title: "Admin Users"), title: "Admin Users"),
     const HomeScreenComponentModel(icon: Icons.person_outline, activeIcon: Icons.person, screen: AdminUserProfileScreen(), title: "Profile"),
   ];
